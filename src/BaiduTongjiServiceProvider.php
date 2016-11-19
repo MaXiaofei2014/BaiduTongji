cca60138b7cb385e21a131247a023cc420614c85
木杉 <804216865@qq.com>
2016-11-19 23:14:36 +0800

    11-19

---
+++ src/BaiduTongjiServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Mushan\BaiduTongji;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaiduTongjiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
