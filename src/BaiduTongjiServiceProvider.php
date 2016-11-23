f37d93cc940781ab828744175e7bdf03ec146fca
木杉 <804216865@qq.com>
2016-11-23 16:59:06 +0800

    11-23

---
+++ src/BaiduTongjiServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mushan
+ * Date: 2016/11/23
+ * Time: 16:57
+ */
+
+namespace Mushan\BaiduTongji;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaiduTongjiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        echo 123;
+    }
+
+    public function register()
+    {
+
+    }
+}
