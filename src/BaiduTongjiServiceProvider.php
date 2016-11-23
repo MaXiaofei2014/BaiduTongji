ea88c4b16fc9752f51c5ecc47fba4e5d6565b2ef
木杉 <804216865@qq.com>
2016-11-23 17:35:03 +0800

    11-23

--- src/BaiduTongjiServiceProvider.php
+++ src/BaiduTongjiServiceProvider.php
@@ -5,20 +5,25 @@
  * Date: 2016/11/23
  * Time: 16:57
  */
-
 namespace Mushan\BaiduTongji;
 
 use Illuminate\Support\ServiceProvider;
 
 class BaiduTongjiServiceProvider extends ServiceProvider
 {
+    protected $defer = true;
+
     public function boot()
     {
-        echo 123;
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('baidu_tongji.php'),
+        ]);
     }
 
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            __DIR__.'/config.php', 'baidu_tongji'
+        );
     }
 }
---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'LOGIN_URL'=>'https://api.baidu.com/sem/common/HolmesLoginService',
+
+    'API_URL'=>'https://api.baidu.com/json/tongji/v1/ReportService',
+
+    /*
+     * 唯一标识
+     */
+    'UUID'=>'mac',
+
+    'TOKEN'=>'',
+
+    'USERNAME'=>'',
+
+    'PASSWORD'=>'',
+
+    /**
+     * 统计用户仅支持如下账户类型：
+    1：站长账号
+    2：凤巢账号
+    3：联盟账号
+    4：哥伦布账号
+     */
+    'account_type'=>1,
+];
