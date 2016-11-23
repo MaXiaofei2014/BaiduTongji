828d9e624f6bbe41a9be7f7693dd5c91e2236537
木杉 <804216865@qq.com>
2016-11-23 16:11:40 +0800

    11-23

---
+++ src/Report.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mushan
+ * Date: 2016/11/23
+ * Time: 16:10
+ */
+
+namespace Mushan\BaiduTongji;
+
+class Report
+{
+    public function __construct()
+    {
+        echo 123;
+    }
+}
